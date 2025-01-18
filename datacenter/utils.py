8fe7ae577d184f6e7265d5bec5738a9531dbb75c
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-01-18 23:55:58 +0500

    Исправил код согласно ревью
    Убрал сравнение if leaved_at is None:

--- datacenter/utils.py
+++ datacenter/utils.py
@@ -6,10 +6,9 @@ SECONDS_IN_HOUR = 3600
 SECONDS_IN_MINUTE = 60
 LONG_VISIT_THRESHOLD = timedelta(hours=1)
 
-def get_duration(entered_at, leaved_at):
+def get_duration(entered_at, leaved_at=None):
     """Возвращает продолжительность визита в секундах."""
-    if leaved_at is None:
-        leaved_at = timezone.now()
+    leaved_at = leaved_at or timezone.now()
     duration = leaved_at - entered_at
     return duration
 
